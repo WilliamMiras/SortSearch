4467aae64e83635c9f82fcdd5edef26520b907fe
William Miras <william.miras@gmail.com>
2025-02-26 22:33:14 -0800

    Added the node class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
+class Node {
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         );
