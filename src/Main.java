f9f37fdc5f952c9996613432201e3cdcd880aeae
William Miras <william.miras@gmail.com>
2025-03-02 20:24:26 -0800

    Completed! All Classes and Sort functions and function calls

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,90 @@
+class sortArray {
 
+    public static void bubbleSort() {
+        int[][] localArray = twoDimArray.copyArray();
+        int COL = 0;
+        int SIZE = localArray.length;
+        int limit = SIZE - 1;
+        int[] temp;
+
+        for( ; limit > 0; limit--) {
+            for(int i = 0; i < limit; i++) {
+                if(localArray[i][COL] > localArray[i+1][COL]) {
+                    temp = localArray[i];
+                    localArray[i] = localArray[i+1];
+                    localArray[i+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nBubble Sort: ");
+        twoDimArray.display(localArray);
+    }
+
+    public static void selectionSort() {
+        int[][] localArray = twoDimArray.copyArray();
+        int COL = 1;
+        int SIZE = localArray.length;
+        int limit = SIZE - 1;
+        int[] temp;
+        int indexOfSmallest;
+
+        for(; limit > 0; limit--) {
+            indexOfSmallest = limit;
+            for(int i = 0; i < limit; i++) {
+                if(localArray[i][COL] < localArray[indexOfSmallest][COL]) {
+                    indexOfSmallest = i;
+                }
+            }
+            if(limit != indexOfSmallest) {
+                temp = localArray[limit];
+                localArray[limit] = localArray[indexOfSmallest];
+                localArray[indexOfSmallest] = temp;
+            }
+        }
+
+        System.out.println("\nSelection Sort: ");
+        twoDimArray.display(localArray);
+    }
+
+    public static void shellSort() {
+        int[][] localArray = twoDimArray.copyArray();
+        int COL = 2;
+        int SIZE = localArray.length;
+        int[] temp;
+
+        for(int gap = SIZE/2; gap >= 1; gap = gap/2) {
+            for(int i = gap; i < SIZE; i++) {
+                for(int j = i; j >= gap && localArray[j-gap][COL] > localArray[j][COL]; j = j - gap) {
+                    temp = localArray[j - gap];
+                    localArray[j - gap] = localArray[j];
+                    localArray[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nShell Sort: ");
+        twoDimArray.display(localArray);
+    }
+
+    public static void insertionSort() {
+        int[][] localArray = twoDimArray.globalArray;
+        int tmp;
+        int ROW = 4;
+        int SIZE = localArray.length - 1;
+
+        for (int i = 1; i < SIZE; i++) {
+            for (int j = i; j > 0 && localArray[ROW][j] < localArray[ROW][j - 1]; j--) {
+                tmp = localArray[ROW][j];
+                localArray[ROW][j] = localArray[ROW][j - 1];
+                localArray[ROW][j - 1] = tmp;
+            }
+        }
+
+        System.out.println("\nInsertion Sort: ");
+        twoDimArray.display(localArray);
+    }
+}
 
 class twoDimArray {
     public static int[][] globalArray = {
@@ -14,6 +100,7 @@ class twoDimArray {
         int mid;
         boolean found = false;
 
+        System.out.println("\nSearching for: " + num);
         while (high >= low && !found) {
             mid = (high + low) / 2;
 
@@ -36,17 +123,41 @@ class twoDimArray {
     }
 
     public static void displayCol(int col) {
-
+        int colFound = col + 1;
+        System.out.println("Found! In Column: [" + colFound + "]");
         for (int i = 0; i <= globalArray[col].length; i++) {
-            System.out.println(globalArray[i][col]);
+            System.out.println("| " + globalArray[i][col] + " |");
         }
         System.out.println();
+    }
+
+    public static void display(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(" | " + array[i][j]);
+            }
+            System.out.println(" |");
+        }
+    }
+
+    public static int[][] copyArray() {
+        int[][] newArray = new int[globalArray.length][globalArray[0].length];
+
+        for(int i = 0; i < globalArray.length; i++) {
+            System.arraycopy(globalArray[i], 0, newArray[i], 0, globalArray[i].length);
+        }
+
+        return newArray;
     }
 
 }
 
 public class Main {
     public static void main(String[] args) {
-            twoDimArray.search(0);
+        sortArray.bubbleSort();
+        sortArray.selectionSort();
+        sortArray.shellSort();
+        sortArray.insertionSort();
+        twoDimArray.search(-1);
     }
 }
